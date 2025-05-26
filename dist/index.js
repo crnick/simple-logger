7f053cdf770df0259000c9861a583a8fea5da801
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2025-05-26 01:41:04 -0400

    created basic folder structure

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// logger.info("Hello World!");  
+// logger.warn("Warning!");  
+// logger.error("Something went wrong!");  
+// logger.success("It works!"); // Optional (not common in other loggers)
+// info → Blue
+// warn → Yellow
+// error → Red
+// success → Green
